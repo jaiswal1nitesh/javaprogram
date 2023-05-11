61c8f2c61cfba27ef0f683af8c8a3da622cf36ca
jaiswal1nitesh <nj639476@gmail.comgit config --global user.email nj639476@gmail.com>
2023-05-11 22:53:28 +0530

    [Nitesh] Program of String  Matching

---
+++ Day1Assignment/StringEqual.java
@@ -0,0 +1,19 @@
+package day1Problem;
+import java.util.*;
+public class StringEqual {
+	public static void main(String[] args) {
+		String nameOriginal ="Nitesh Jaiswal";
+		String name2 = "Nitesh Jaiswal";
+		Scanner sc = new Scanner(System.in);
+		//System.out.println("Enter your Name");
+		//String name1 = sc.nextLine();
+		if(nameOriginal == name2)
+		{
+			System.out.println("Your Name is Vaildate");
+	   }  
+		else
+		{
+			System.out.println("Your Name is Not Validate");
+		}	
+	}
+}
