103e4b62523860f2156eb2bf9ff61b8728e56246
jaiswal1nitesh <nj639476@gmail.comgit config --global user.email nj639476@gmail.com>
2023-05-11 22:52:08 +0530

    [Nitesh] Print Hello

---
+++ Day1Assignment/DisplayMsg.java
@@ -0,0 +1,11 @@
+package day1Problem;
+
+public class DisplayMsg {
+
+	public static void main(String[] args) {
+		System.out.println("Hello , My name is Nitesh Jaiswal");
+		System.out.println("welcome");
+
+	}
+
+}
