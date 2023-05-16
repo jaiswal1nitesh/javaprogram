55fb18de40a1dad9b07a8eafa43aba2d4fe1273f
jaiswal1nitesh <nj639476@gmail.comgit config --global user.email nj639476@gmail.com>
2023-05-16 08:16:57 +0530

    Program of find vowel and consonant

---
+++ day2Assignment/switchCase/VowelConsonant.java
@@ -0,0 +1,33 @@
+package switchCase;
+
+import java.util.Scanner;
+
+public class VowelConsonant {
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter The Alphabet");
+		char  ch =sc.next().charAt(0);
+		switch (ch) {
+		case 'a':
+			System.out.println("vowel");
+			break;
+		case 'e':
+			System.out.println("vowel");
+			break;
+		case 'o':
+			System.out.println("vowel");
+			break;
+		case 'i':
+			System.out.println("vowel");
+			break;
+		case 'u':
+			System.out.println("vowel");
+			break;
+		default :
+			System.out.println("Consonant");
+					}
+
+	}
+
+}
