a8f5215f67493ce756330a7e8d5a19846816319c
jaiswal1nitesh <nj639476@gmail.comgit config --global user.email nj639476@gmail.com>
2023-05-16 08:19:53 +0530

    Simple Switch case

---
+++ day2Assignment/switchCase/Test.java
@@ -0,0 +1,17 @@
+package switchCase;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int c=1;
+		switch (c) {
+		case 1:
+			System.out.println("one"+(c>6));
+			break;
+		//case (c>6 = 'false'):
+			//System.out.println("Mission done");
+		}
+
+	}
+
+}
